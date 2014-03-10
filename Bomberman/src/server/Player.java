126c169b265a07f353b07a694bf8a54384b99b1b
AndrewDeCHamplain <andrewdechamplain@cmail.carleton.ca>
2014-03-10 13:17:26 -0400

    update player

--- Bomberman/src/server/Player.java
+++ Bomberman/src/server/Player.java
@@ -3,9 +3,9 @@ import java.net.InetAddress;
 
 
 public class Player {
-	int x, y, lives; 
-	char playerNumber;
-	InetAddress IP;
+	private int x, y, lives; 
+	private char playerNumber;
+	private InetAddress IP;
 
 	Player(int x,int y,char num,InetAddress IP){
 		this.x = x;
@@ -28,5 +28,17 @@ public class Player {
 	}
 	public InetAddress getIPAddress(){
 		return IP;
+	}
+	public void setX(int x){
+		this.x=x;
+	}
+	public void setY(int y){
+		this.y=y;
+	}
+	public void setLives(int lives){
+		this.lives = lives;
+	}
+	public void setPlayerNumber(char playerNum){
+		playerNumber = playerNum;
 	}
 }
