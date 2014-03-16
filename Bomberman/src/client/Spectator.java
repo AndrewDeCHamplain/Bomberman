02dd7e458b4be039111a7686136c6ced085d73d9
bakshbhandal <baksh.bhandal@carleton.ca>
2014-03-15 20:01:58 -0400

    Update Spectator.java
    Explained method. with comments

--- Bomberman/src/client/Spectator.java
+++ Bomberman/src/client/Spectator.java
@@ -1,7 +1,21 @@
 package client;
 
-public class Spectator {
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
+
+@SuppressWarnings("serial")
+public class Spectator extends GameView {
+/*
+ * This method is a call constructor to allow invitation for SPECIFICALLY spectator players.
+ * I.e.: If a player starts the game, he/she has an option to join as spectator from the start
+ * OR can become a spectator after having lost the game.
+	public Spectator(ArrayList<ArrayList<Character>> args, char playerNum,
+			Semaphore semaphore) {
+		super(args, playerNum, semaphore);
+		// TODO Auto-generated constructor stub
+	}
+*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
