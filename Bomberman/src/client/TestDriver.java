6dc289ee617a51d085346053d7dca2f74f5d8e7c
Chris Sparrow <chrissparrow@cmail.carelton.ca>
2014-03-17 13:43:22 -0400

    Tests from a text file now. One example test file has been put in resources

--- Bomberman/src/client/TestDriver.java
+++ Bomberman/src/client/TestDriver.java
@@ -1,19 +1,41 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.FileInputStream;
 
 public class TestDriver {
 
 	
+	//The text file containing the test inputs is stored in testFile
+	private static BufferedReader testFile;
+	
 	static BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
 			System.in));
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		
 		String sentence = "";
 		
+		while(true){
+			System.out.println("enter the name of a testing file (do not type the extension): ");
+			try {
+				sentence = inFromUser.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(sentence.equals("quit"))
+				break;
+			else {
+				test(sentence);
+			}
+		}
+		
+		/*
 		System.out.println("Choose test to be performed. (1,2, or 3) Type 'quit' to end test");
 		while(true){
 			try {
@@ -35,8 +57,32 @@ public class TestDriver {
 			if(sentence.equals("quit"))
 				break;
 		}
+		*/
 	}
 	
+	private static void test(String testCase) throws IOException, InterruptedException {
+		FileInputStream in = new FileInputStream("resources/" + testCase + ".txt");
+		
+		testFile = new BufferedReader(new InputStreamReader(in));
+		
+		String testInput = testFile.readLine();
+		
+		Thread client1 = new Thread(new ClientTest());
+		client1.start();
+		
+		while (testInput != null) {
+			if(testInput.equals("sleep")) {
+				Thread.sleep(500);
+			}
+			else {
+				Client.setCurrMove(testInput);
+			}
+			Thread.sleep(500);
+			testInput = testFile.readLine();
+		}
+		in.close();
+	}
+	/*
 	private static void test1() throws InterruptedException{
 		
 		Thread client1 = new Thread(new ClientTest());
@@ -60,4 +106,5 @@ public class TestDriver {
 		Client.setCurrMove("up");
 
 	}
+	*/
 }
