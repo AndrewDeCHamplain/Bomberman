881b10f9d15787a66e403b708f359b3f750953b9
AndrewDeCHamplain <andrewdechamplain@cmail.carleton.ca>
2014-03-29 20:55:21 -0400

    PlayerSleep added

---
+++ Bomberman/src/server/PlayerSleep.java
@@ -0,0 +1,23 @@
+package server;
+
+public class PlayerSleep implements Runnable{
+
+	private Player player;
+	
+	public PlayerSleep(Player player){
+		this.player = player;
+	}
+	@Override
+	public void run() {
+		player.setPlayerStatus(false);
+		// TODO Auto-generated method stub
+		try {
+			Thread.sleep(800);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		player.setPlayerStatus(true);
+	}
+
+}
